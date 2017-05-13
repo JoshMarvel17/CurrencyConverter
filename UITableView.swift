8928b0a9706b83992e15c7c381e378f3c56f9e0b
Josh Marvel <joshamarvel@gmail.com>
2017-05-12 21:52:23 -0700

    CurrencyConverter
    Assignment 4 for 411

---
+++ UITableView.swift
@@ -0,0 +1,9 @@
+//
+//  UITableView.swift
+//  currencyConverter
+//
+//  Created by Joshua Marvel on 5/12/17.
+//  Copyright © 2017 Joshua Marvel. All rights reserved.
+//
+
+import Foundation
---
+++ currencyConverter/UIPickerView.swift
@@ -0,0 +1,61 @@
+//
+//  UIPickerView.swift
+//  currencyConverter
+//
+//  Created by Joshua Marvel on 5/12/17.
+//  Copyright © 2017 Joshua Marvel. All rights reserved.
+//
+
+
+import UIKit
+
+class PickerViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let swipeLeft:UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.left
+        view.addGestureRecognizer(swipeLeft)
+
+        
+        //colorPickerView.dataSource = self
+        //colorPickerView.delegate = self
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func handleSwipe(_ sender:UISwipeGestureRecognizer)
+    {
+        //getInput()
+        //discountData.Calculate()
+        //displayTotals()
+        self.performSegue(withIdentifier: "showResults", sender: self)
+    }
+    
+    //Enable unwinding other views
+    @IBAction func unwindToCalc(segue:UIStoryboardSegue){}
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int{
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int )-> Int {
+        //code
+        //return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        //code
+        //return pickerData[row]
+
+    }
+
+}
